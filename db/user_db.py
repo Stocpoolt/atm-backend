746a69e8a1bd53f6ab95e676425c7184f1a82f60
Stocpoolt <stocpoolt@gmail.com>
2021-01-30 22:53:16 -0500

    Jan30 22:53

--- db/user_db.py
+++ db/user_db.py
@@ -1,27 +1,11 @@
-from typing import Dict
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from db.db_connection import Base, engine
 
-class UserInDB(BaseModel):
-    username: str
-    password: str
-    balance: int
+class UserInDB(Base):
+    __tablename__ = "users"
 
-database_users = Dict[str, UserInDB]
-database_users = {
-    "carlos24": UserInDB(**{"username":"carlos24",
-                            "password":"root",
-                            "balance":12000}),
-    "andres24": UserInDB(**{"username":"andres24",
-                            "password":"root",
-                            "balance":34000}),
-}
+    username    = Column(String, primary_key=True, unique=True)
+    password    = Column(String)
+    balance     = Column(Integer)
 
-def get_user(username: str):
-    if username in database_users.keys():
-        return database_users[username]
-    else:
-        return None
-
-def update_user(user_in_db: UserInDB):
-    database_users[user_in_db.username] = user_in_db
-    return user_in_db
+Base.metadata.create_all(bind=engine)
--- models/user_models.py
+++ models/user_models.py
@@ -7,3 +7,6 @@ class UserIn(BaseModel):
 class UserOut(BaseModel):
     username    : str
     balance     : int
+
+    class Config:
+        orm_mode = True
