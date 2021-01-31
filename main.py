3c0814269fea167b16ea408f7539ea9348ee5202
Stocpoolt <stocpoolt@gmail.com>
2021-01-31 17:12:56 -0500

    Jan31 17:12

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import Depends, FastAPI
 from routers.user_router        import router as router_users  
 from routers.transaction_router import router as router_transactions
 
+
 api = FastAPI()
 
 from fastapi.middleware.cors import CORSMiddleware
@@ -13,9 +14,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:8081",
-    "http://localhost:8082",
-    "http://localhost:8083",
-    "https://stocpoolt-atm-backend.herokuapp.com",
+    "https://stocpoolt-atm-frontend.herokuapp.com",
 ]
 api.add_middleware(
     CORSMiddleware, allow_origins=origins,
